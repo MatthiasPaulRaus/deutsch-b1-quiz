ce209177926d71fd67511b6096c02b68a43a8d60
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-08-04 18:11:52 +0200

    first commit

---
+++ src/List.jsx
@@ -0,0 +1,12 @@
+import ListB1eins from './ListB1eins'
+import ListB1zwei from './ListB1zwei'
+import ListB1drei from './ListB1drei'
+import ListB1vier from './ListB1vier'
+
+const List = [];
+List[0]= [ListB1eins]
+List[1]= [ListB1zwei]
+List[2]= [ListB1drei]
+List[3]= [ListB1vier]
+
+export default List
---
+++ src/Stufen.jsx
@@ -0,0 +1,7 @@
+const Stufen = [
+    {name:"B1-eins"},
+    {name:"B1-zwei"},
+    {name:"B1-drei"},
+    {name:"B1-vier"}
+    ]
+export default Stufen  
